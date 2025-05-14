cc052433f1947093f8e46da6382b399ef12ca575
Dizos <tandomjekula@gmail.com>
2025-05-14 10:48:12 +0200

    update

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,25 +1,42 @@
 #!/usr/bin/env python3
-""" Basic caching module
+""" FIFO caching module
 """
 
 from base_caching import BaseCaching
 
 
-class BasicCache(BaseCaching):
-    """ BasicCache defines a caching system that:
+class FIFOCache(BaseCaching):
+    """ FIFOCache defines a FIFO caching system that:
         - Inherits from BaseCaching
-        - Stores items in a dictionary without any size limit
-        - Allows adding and retrieving items by key
+        - Stores items in a dictionary with a maximum limit
+        - Discards the first item added when the cache is full
     """
 
+    def __init__(self):
+        """ Initialize the FIFO cache
+        """
+        super().__init__()
+        self.order = []  # Tracks insertion order for FIFO
+
     def put(self, key, item):
-        """ Add an item to the cache
+        """ Add an item to the cache using FIFO strategy
             Args:
                 key: The key for the item
                 item: The item to be stored
         """
-        if key is not None and item is not None:
-            self.cache_data[key] = item
+        if key is None or item is None:
+            return
+
+        # Add or update the item in cache
+        if key not in self.cache_data:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Remove the first item added (FIFO)
+                first_key = self.order.pop(0)
+                print(f"DISCARD: {first_key}")
+                del self.cache_data[first_key]
+            self.order.append(key)
+        
+        self.cache_data[key] = item
 
     def get(self, key):
         """ Retrieve an item from the cache by key
